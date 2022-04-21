299368623dd7904fd33fabf6dfdbafd952cab24d
Nicholas Hacault <nicholashacaulttabs@gmail.com>
2022-04-21 16:37:55 -0500

    Add logic to process all html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { src, dest, series, parallel } = require('gulp');
+const gulpWebpack = require('webpack-stream');
+const cleanCSS = require('clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+const processIndex = () => {
+  return src('src/index.html').pipe(dest('dist'));
 }
 
-exports.default = defaultTask
+const processPages = () => {
+  return src('src/pages/*.html').pipe(dest('dist/pages'));
+}
+
+const processBlogPages = () => {
+  return src('src/pages/blog-pages/*.html').pipe(dest('dist/pages/blog-pages'));
+}
+ 
+exports.default = series(processIndex, processPages, processBlogPages);
